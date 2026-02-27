ad0e547d0e3f3f594b53d43b4b8200959f8e501c
antek-code <234240847+antek-code@users.noreply.github.com>
2026-02-27 11:11:57 +0100

    Add files via upload

--- 1_intro.cpp
+++ 1_intro.cpp
@@ -10,12 +10,29 @@ Multi-line comment:
 - argc/argv: command-line args (ignore for now).
 */
 
+int subtract(int a, int b)
+{
+    int result;
+    result = a - b;
+    return result;
+}
+
 int main() {  // Program starts here.
     // std::cout << "Hello, World!" << std::endl;  // Step 1: Basic output.
     // Uncomment above, build/run (Ctrl+F5 in VS).
 
     // std::cout << "My name is " << "Student" << std::endl;  // Step 2: Concat.
     // std::cout << "2 + 3 = " << (2 + 3) << std::endl;       // Step 3: Expression.
+
+    int wynik, liczba_a, liczba_b;
+
+    std::cout << "Podaj pierwszą liczbę: ";
+    std::cin >> liczba_a;
+    std::cout << "Podaj drugą liczbę: ";
+    std::cin >> liczba_b;
+
+    wynik = subtract(liczba_a, liczba_b);
+    std::cout << wynik;
 
     return 0;  // Exit cleanly.
 }
